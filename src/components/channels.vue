0d44f2f02e4930e526f12e06af0698c2d0a55036
nutmmm <nutmmm@organicdesign.co.nz>
2018-03-22 11:07:45 +1300

    Added the components and style from the master branch.

---
+++ src/components/channels.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="channelListContainer">
+		<ul class="channelList">
+			<li v-for="channel in channels" :key="channel._id" @click="changeChannel(channel)" class="channel">
+				{{ channel.name }}
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			channels: Array,
+			current: Object
+		},
+		methods: {
+			changeChannel(channel) {
+				if (this.current !== channel) {
+					this.$emit('selectChannel', channel);
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+	.channelList{
+		display: flex;
+		flex-direction: column;
+		padding: 0px;
+		margin: 0px;
+		height:100%;
+	}
+
+	.channelListContainer{
+		height:100%;
+    	width: 115px;
+	}
+
+	.channel{
+		border: 1px solid black;
+		border-radius: 100px;
+		padding-left: auto;
+		padding-right: auto;
+		width: 100px;
+		height: 100px;
+		margin: 5px;
+		cursor: pointer;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+	}
+</style>
--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -1,13 +1,24 @@
 <template>
-	<div>
-		<span @click="Logout()">
-			test
-		</span>
+	<div class="chatContainer">
+		<div class="logoutButton" @click="Logout()">
+			Logout
+		</div>
+		<!--<channels :channels="channels" :current="currentChannel" @selectChannel="onSelectChannel"></channels>-->
+		<channels></channels>
+		<div class="right">
+			<!--<chatHistory :messages="messages"></chatHistory>-->
+			<!--<chat @sendMessage="sendMessage"></chat>-->
+			<chatHistory></chatHistory>
+			<chat></chat>
+		</div>
 	</div>
 </template>
 
 <script>
-	import auth from '../libs/auth'
+	import auth from '../libs/auth';
+	import Channels from '../components/channels.vue'
+	import ChatHistory from '../components/chatHistory.vue'
+	import Chat from '../components/chat.vue'
 
 	export default {
 		methods: {
@@ -15,9 +26,42 @@
 				auth.logout();
 				this.$router.push("/login");
 			}
+		},
+
+		components: {
+			Channels,
+			ChatHistory,
+			Chat
 		}
 	}
 </script>
 
 <style>
+	.chatContainer{
+		display: flex;
+  		flex-wrap: wrap;
+		padding: 0px;
+		margin: 0px;
+		width: 100%;
+		height: 100%;
+	}
+
+	.right{
+		height: 100%;
+    	width: calc(100% - 120px);
+	}
+
+	.logoutButton{
+		position: fixed;
+		right: 30px;
+		top: 30px;
+		border: 1px solid black;
+		border-radius: 100px;
+		width:90px !important;
+		height: 90px !important;
+		cursor: pointer;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
 </style>
