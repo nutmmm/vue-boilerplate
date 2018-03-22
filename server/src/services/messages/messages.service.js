97ab8e549bef5bc37cf953a7e4f6ad4440236dbc
nutmmm <nutmmm@organicdesign.co.nz>
2018-03-22 16:12:30 +1300

    Removed clutter.

--- server/src/services/messages/messages.service.js
+++ server/src/services/messages/messages.service.js
@@ -1,23 +1,23 @@
-// Initializes the `chat` service on path `/chat`
+// Initializes the `messages` service on path `/messages`
 const createService = require('feathers-mongoose');
-const createModel = require('../../models/chat.model');
-const hooks = require('./chat.hooks');
+const createModel = require('../../models/messages.model');
+const hooks = require('./messages.hooks');
 
 module.exports = function (app) {
   const Model = createModel(app);
   const paginate = app.get('paginate');
 
   const options = {
-    name: 'chat',
+    name: 'messages',
     Model,
     paginate
   };
 
   // Initialize our service with any options it requires
-  app.use('/chat', createService(options));
+  app.use('/messages', createService(options));
 
   // Get our initialized service so that we can register hooks and filters
-  const service = app.service('chat');
+  const service = app.service('messages');
 
   service.hooks(hooks);
 };
